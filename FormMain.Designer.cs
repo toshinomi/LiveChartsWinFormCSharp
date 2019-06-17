be8f453bcea48ac06eac65f1822d6aebbffef3cc
達也 鴛海 <tatsuya.oshinomi.0828@gmail.com>
2019-06-18 01:02:16 +0900

    プロジェクト ファイルを追加します。

---
+++ FormMain.Designer.cs
@@ -0,0 +1,88 @@
+﻿namespace LiveChartsWinFormCSharp
+{
+    partial class FormMain
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージド リソースを破棄する場合は true を指定し、その他の場合は false を指定します。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.chart = new LiveCharts.WinForms.CartesianChart();
+            this.image = new System.Windows.Forms.PictureBox();
+            this.btnFileSelect = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.image)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // chart
+            // 
+            this.chart.Location = new System.Drawing.Point(360, 12);
+            this.chart.Name = "chart";
+            this.chart.Size = new System.Drawing.Size(428, 426);
+            this.chart.TabIndex = 1;
+            // 
+            // image
+            // 
+            this.image.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.image.Location = new System.Drawing.Point(12, 52);
+            this.image.Name = "image";
+            this.image.Size = new System.Drawing.Size(325, 371);
+            this.image.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.image.TabIndex = 2;
+            this.image.TabStop = false;
+            // 
+            // btnFileSelect
+            // 
+            this.btnFileSelect.BackColor = System.Drawing.Color.White;
+            this.btnFileSelect.Location = new System.Drawing.Point(12, 12);
+            this.btnFileSelect.Name = "btnFileSelect";
+            this.btnFileSelect.Size = new System.Drawing.Size(84, 32);
+            this.btnFileSelect.TabIndex = 3;
+            this.btnFileSelect.Text = "File Select...";
+            this.btnFileSelect.UseVisualStyleBackColor = false;
+            this.btnFileSelect.Click += new System.EventHandler(this.OnClickBtnFileSelect);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.LightSteelBlue;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnFileSelect);
+            this.Controls.Add(this.image);
+            this.Controls.Add(this.chart);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.image)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private LiveCharts.WinForms.CartesianChart chart;
+        private System.Windows.Forms.PictureBox image;
+        private System.Windows.Forms.Button btnFileSelect;
+    }
+}
+
---
+++ FormMain.cs
@@ -0,0 +1,120 @@
+﻿using LiveCharts;
+using LiveCharts.Wpf;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Windows.Media.Imaging;
+
+namespace LiveChartsWinFormCSharp
+{
+    public partial class FormMain : Form
+    {
+        private int[] m_nHistgram;
+
+        public FormMain()
+        {
+            InitializeComponent();
+
+            m_nHistgram = new int[256];
+
+            InitGraph();
+        }
+
+        private void OnClickBtnFileSelect(object sender, EventArgs e)
+        {
+            ComOpenFileDialog openFileDlg = new ComOpenFileDialog();
+            openFileDlg.Filter = "JPG|*.jpg|PNG|*.png";
+            openFileDlg.Title = "Open the file";
+            if (openFileDlg.ShowDialog() == true)
+            {
+                image.Image = null;
+                var bitmap = new Bitmap(openFileDlg.FileName);
+                image.Image = bitmap;
+
+                DrawHistgram(bitmap);
+            }
+            return;
+        }
+
+        public void InitGraph()
+        {
+            LineSeries lineSeriesChart = new LineSeries()
+            {
+                Values = new ChartValues<int>(),
+                Title = "Histgram"
+            };
+
+            for (int nIdx = 0; nIdx < 256; nIdx++)
+            {
+                lineSeriesChart.Values.Add(0);
+            }
+            chart.Series.Clear();
+            chart.Series.Add(lineSeriesChart);
+
+            return;
+        }
+
+        public void DrawHistgram(Bitmap _bitmap)
+        {
+            InitHistgram();
+
+            CalHistgram(_bitmap);
+
+            LineSeries lineSeriesChart = new LineSeries()
+            {
+                Values = new ChartValues<int>(),
+                Title = "Histgram"
+            };
+
+            for (int nIdx = 0; nIdx < 256; nIdx++)
+            {
+                lineSeriesChart.Values.Add(m_nHistgram[nIdx]);
+            }
+            chart.Series.Clear();
+            chart.Series.Add(lineSeriesChart);
+
+            return;
+        }
+
+        public void CalHistgram(Bitmap _bitmap)
+        {
+            int nWidthSize = _bitmap.Width;
+            int nHeightSize = _bitmap.Height;
+
+            BitmapData bitmapData = _bitmap.LockBits(new Rectangle(0, 0, nWidthSize, nHeightSize), ImageLockMode.ReadWrite, PixelFormat.Format32bppArgb);
+
+            int nIdxWidth;
+            int nIdxHeight;
+
+            unsafe
+            {
+                for (nIdxHeight = 0; nIdxHeight < nHeightSize; nIdxHeight++)
+                {
+                    for (nIdxWidth = 0; nIdxWidth < nWidthSize; nIdxWidth++)
+                    {
+                        byte* pPixel = (byte*)bitmapData.Scan0 + nIdxHeight * bitmapData.Stride + nIdxWidth * 4;
+                        byte nGrayScale = (byte)((pPixel[(int)ComInfo.Pixel.B] + pPixel[(int)ComInfo.Pixel.G] + pPixel[(int)ComInfo.Pixel.R]) / 3);
+
+                        m_nHistgram[nGrayScale] += 1;
+                    }
+                }
+                _bitmap.UnlockBits(bitmapData);
+            }
+        }
+
+        public void InitHistgram()
+        {
+            for (int nIdx = 0; nIdx < 256; nIdx++)
+            {
+                m_nHistgram[nIdx] = 0;
+            }
+        }
+    }
+}
